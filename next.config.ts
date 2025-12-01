e9ce9d252082b1dd15095e92bab0d4a6553fd81f
raatook <tokyfreel@gmail.com>
2025-12-01 21:00:54 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -7,28 +7,7 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.vitasoft.mg',
-                pathname: '/**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                pathname: '**',
-            },
-        ],
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-            },
-        ];
-    },
+    images: {}
 };
 
 export default nextConfig;
